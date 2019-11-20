7f7fb13b62c7968ac363570fe1acd837549d96b6
TommyAndersson1991 <tommy.andersson@iths.se>
2019-11-20 15:26:12 +0100

    fixed commit

--- C_Labb2/Position.cs
+++ C_Labb2/Position.cs
@@ -1,14 +1,121 @@
-﻿namespace C_Labb2
+﻿using System;
+
+namespace C_Labb2
 {
     internal class Position
     {
-        private int v1;
-        private int v2;
+        public int x;
+        public int y;
 
-        public Position(int v1, int v2)
+        public int X
         {
-            this.v1 = v1;
-            this.v2 = v2;
+            get => x;
+            set => x = Math.Abs(value);
+        }
+
+        public int Y
+        {
+            get => y;
+            set => y = Math.Abs(value);
+        }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y; 
+        }
+
+        public double Length()
+        {
+           var origoDistance = Math.Sqrt(x * x + y * y);
+            return origoDistance;
+        }
+
+        public bool Equals(Position p)
+        {
+            if (p.x == p.y)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Position Clone()
+        {
+            return new Position(x, y); 
+        }
+
+        public override string ToString()
+        {
+            return $"({x},{y})";
+        }
+
+        public static bool operator >(Position p1, Position p2)
+        {
+            if (p1.Length() > p2.Length())
+            {
+                return true;
+            }if (p2.Length() > p1.Length())
+            {
+                return false; 
+            }
+
+            {
+                if (p1.x > p2.x)
+                {
+                    return true; 
+                }
+                {
+                    return false;
+                }
+            }
+            
+                    }
+
+        public static bool operator <(Position p1, Position p2)
+        {
+            if (p1.Length() < p2.Length())
+            {
+                return true; 
+            }
+            if (p2.Length() < p1.Length())
+            {
+                return false;
+            }
+            if (p1.x < p2.x)
+            {
+                return true;
+            }
+            {
+                return false;
+            }
+
+        }
+
+        public static Position operator +(Position p1, Position p2)
+        {
+           
+           var newX = p1.x + p2.x;
+           var newY = p1.y + p2.y;
+            return new Position(newX, newY);
+
+        }
+
+        public static Position operator -(Position p1, Position p2)
+        {
+
+            var newX = p1.x - p2.x;
+            var newY = p1.y - p2.y;
+            return new Position(newX, newY);
+
+        }
+
+        public static double operator %(Position p1, Position p2)
+        {
+            var x = Math.Pow(p1.x - p2.x, 2);
+            var y = Math.Pow(p1.y - p2.y, 2);
+            var distance = Math.Sqrt(x + y);
+            return distance;
         }
     }
 }
--- C_Labb2/SortedPosList.cs
+++ C_Labb2/SortedPosList.cs
@@ -1,6 +1,115 @@
-﻿namespace C_Labb2
+﻿using System.Collections.Generic;
+using System;
+namespace C_Labb2
 {
     internal class SortedPosList
     {
+        public List<Position> positionList = new List<Position>();
+
+
+        public SortedPosList()
+        {
+            positionList.Sort((x, y) => x.Length().CompareTo(y.Length()));
+
+
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", positionList);
+        }
+
+        public int Count()
+        {
+            return positionList.Count;
+        }
+
+        public void Add(Position pos)
+        {
+            positionList.Add(pos);
+            positionList.Sort((x, y) => x.Length().CompareTo(y.Length()));
+        }
+
+        public bool Remove(Position pos)
+        {
+            foreach (var positionItem in positionList)
+            {
+                if (pos.X == positionItem.X && pos.Y == positionItem.Y)
+                {
+                    positionList.Remove(positionItem);
+                    return true;
+                }
+            }
+            return false;
+
+        }
+        public SortedPosList Clone()
+        {
+            SortedPosList clonedList = new SortedPosList();
+            foreach (var position in positionList)
+            {
+                clonedList.Add(position.Clone());
+            }
+            return clonedList;
+        }
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            SortedPosList circleList = Clone();
+
+            for (int i = 0; i < positionList.Count; i++)
+            { 
+                Position position = positionList[i];
+                bool isOutside = Math.Pow(position.X - centerPos.X, 2) + Math.Pow(position.Y - centerPos.Y, 2) > Math.Pow(radius, 2);
+                if (isOutside)
+                {
+                    circleList.Remove(position);
+                }
+
+            }
+            return circleList;
+        }
+
+        public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList list = sp1.Clone();
+            foreach (var position in sp2.positionList)
+            {
+                list.Add(position);
+            }
+
+            return list;
+        }
+
+        public Position this[int index] => positionList[index];
+
+
+
+        public static SortedPosList operator -(SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList filteredList = sp1.Clone();
+
+            int list1Index = 0;
+            int list2Index = 0;
+
+            while (list1Index < sp1.Count() && list2Index < sp2.Count())
+            {
+                if (sp1.positionList[list1Index].Equals(sp2.positionList[list2Index]))
+                {
+                    filteredList.Remove(sp1.positionList[list1Index]);
+                    list1Index++;
+                    list2Index++;
+                }
+                else if (sp1.positionList[list1Index] < sp2.positionList[list2Index])
+                {
+                    list1Index++;
+                }
+                else
+                {
+                    list2Index++;
+                }
+            }
+            return filteredList;
+        }
     }
 }
+
