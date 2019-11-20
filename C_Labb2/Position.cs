97d83c98c160a0857584ce5b0ff5539230a63d74
TommyAndersson1991 <tommy.andersson@iths.se>
2019-11-20 15:24:05 +0100

    commit

---
+++ C_Labb2/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace C_Labb2
+{
+    internal class Position
+    {
+        private int v1;
+        private int v2;
+
+        public Position(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+}
